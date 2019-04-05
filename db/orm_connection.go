a7b3ca5c09fea3183f6b9d1f21b8f3f97eba15d3
Stephen <liwp.stephen@gmail.com>
2019-04-05 10:19:31 +0800

    Change database function.

--- db/orm_connection.go
+++ db/orm_connection.go
@@ -8,37 +8,22 @@ import (
 	"github.com/swanwish/go-common/logs"
 )
 
-/**
- * Get connection get function by configure id
- */
-func GetOrmConnectionGetFunc(id string) func() (*xorm.Engine, error) {
-	return func() (*xorm.Engine, error) {
-		driver, _ := config.Get(fmt.Sprintf("db_driver_%s", id))
-		dsn, _ := config.Get(fmt.Sprintf("db_dsn_%s", id))
-		if driver == "" || dsn == "" {
-			logs.Errorf("The driver or dsn is not specified for id %s.", id)
-			return nil, ErrConfigurationMissing
-		}
-		db, err := xorm.NewEngine(driver, dsn)
-		return db, err
+func GetOrmDBConnection(id string) (*xorm.Engine, error) {
+	driver, _ := config.Get(fmt.Sprintf("db_driver_%s", id))
+	dsn, _ := config.Get(fmt.Sprintf("db_dsn_%s", id))
+	if driver == "" || dsn == "" {
+		logs.Errorf("The driver or dsn is not specified for id %s.", id)
+		return nil, ErrConfigurationMissing
 	}
+	db, err := xorm.NewEngine(driver, dsn)
+	return db, err
 }
 
-func GetOrmConnectionWithDriverAndDsn(driver, dsn string) func() (*xorm.Engine, error) {
-	return func() (*xorm.Engine, error) {
-		if driver == "" || dsn == "" {
-			logs.Errorf("The driver or dsn is not specified.")
-			return nil, ErrConfigurationMissing
-		}
-		db, err := xorm.NewEngine(driver, dsn)
-		return db, err
+func GetOrmDBConnectionWithDriverAndDsn(driver, dsn string) (*xorm.Engine, error) {
+	if driver == "" || dsn == "" {
+		logs.Errorf("The driver or dsn is not specified.")
+		return nil, ErrConfigurationMissing
 	}
-}
-
-func GetOrmDBConnection(id string) DB {
-	return DefaultDB{ConnectionGetterFunc: GetConnectionGetFunc(id)}
-}
-
-func GetOrmDBConnectionWithDriverAndDsn(driver, dsn string) DB {
-	return DefaultDB{ConnectionGetterFunc: GetConnectionWithDriverAndDsn(driver, dsn)}
+	db, err := xorm.NewEngine(driver, dsn)
+	return db, err
 }
