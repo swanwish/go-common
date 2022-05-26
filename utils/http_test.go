3051634f794f85e8b7ec8c48c205099eda5855d2
Stephen <stephen@wdbible.org>
2022-05-26 09:31:28 +0700

    Update test code got get request

--- utils/http_test.go
+++ utils/http_test.go
@@ -2,7 +2,13 @@ package utils
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetRequest(t *testing.T) {
+	requestUrl := "https://jsonplaceholder.typicode.com/todos/1"
+	_, content, err := GetRequest(requestUrl, nil, nil, nil)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, content)
 }
