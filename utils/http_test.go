48010f8e5f455506fd4694c21494e68fa4da2c5e
Stephen <stephen@wdbible.org>
2022-05-26 08:40:41 +0700

    Upgrade viper version

--- utils/http_test.go
+++ utils/http_test.go
@@ -1,19 +1,8 @@
 package utils
 
 import (
-	"net/url"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestGetRequest(t *testing.T) {
-	requestUrl := "https://4.dbt.io/api/download/list"
-	querys := url.Values{}
-	querys.Set("v", "4")
-	querys.Set("key", "1462b719-42d8-0874-7c50-905063472458")
-	querys.Set("page", "2")
-	_, content, err := GetRequest(requestUrl, nil, querys, nil)
-	assert.Nil(t, err)
-	assert.NotEmpty(t, content)
 }
